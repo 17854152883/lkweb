b0851d075de77418a6417e50992eeed581fd1325
like <17854152883@163.com>
2021-01-06 22:52:51 +0800

    增加未登录跳转登录页面

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import router from '../router'
 
 const instance = axios.create({
     baseURL: process.env.BASE_API,
@@ -16,8 +17,50 @@ axios.interceptors.request.use(
         return confing
     },
     (error) => {
+        toLogin()
         return Promise.reject(error)
     }
 )
+
+axios.interceptors.response.use(
+    (error) => {
+        toLogin()
+        return Promise.reject(error)
+    }
+)
+
+const toLogin = () => {
+    router.replace({
+        path: '/login',
+        query: {
+            redirect: router.currentRoute.fullPath
+        }
+    });
+}
+
+const errorHandle = (status, other) => {
+    // 状态码判断
+    switch (status) {
+    // 401: 未登录状态，跳转登录页   
+        case 401:
+        toLogin();
+        break;
+    // 403 token过期
+    // 清除token并跳转登录页
+        case 403:
+        tip('登录过期，请重新登录');
+        localStorage.removeItem('token');
+        store.commit('loginSuccess', null);
+        setTimeout(() => {
+              toLogin();
+        }, 1000);
+       break;
+   // 404请求不存在
+        case 404:
+        tip('请求的资源不存在');
+      break;
+      default:
+      console.log(other);
+}}
 
 export default axios
